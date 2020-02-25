6f878fa2615092a21496befde83466d27efde427
mholwill <m_holwill@hotmail.co.uk>
2020-02-25 14:22:10 +0000

    adds globalPop()

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ document.addEventListener('DOMContentLoaded', () =>{
         const resquest = fetch("https://restcountries.eu/rest/v2/all")
         .then(response => response.json())
         .then(data => this.countries = data)
+      },
+      globalPop: function () {
+        return this.countries.reduce((total, country) => {
+          return total += country.population
+        },0)
       }
     }
   })
